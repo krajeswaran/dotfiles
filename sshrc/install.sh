0abc4390540921e0f743a0211509a2f4aea73665
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2017-06-06 15:20:10 +0530

    removing sshrc to master

--- sshrc/install.sh
+++
@@ -1,4 +0,0 @@
-curl -fsSL "https://github.com/krajeswaran/dotfiles/raw/master/sshrc/.bashrc" >> ~/.bashrc
-curl -fsSL "https://github.com/krajeswaran/dotfiles/raw/master/sshrc/.zshrc" >> ~/.zshrc
-curl -fsSL "https://github.com/krajeswaran/dotfiles/raw/master/sshrc/.tmux.conf" > ~/.tmux.conf
-curl -fsSL "https://github.com/krajeswaran/dotfiles/raw/master/sshrc/.vimrc" > ~/.vimrc
