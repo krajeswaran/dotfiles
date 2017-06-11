413176459834d9c75f68c5373fe0ceb56341ca49
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2017-06-11 16:13:02 +0530

    adding moar

--- bin/bin/setup-post-os.sh
+++ bin/bin/setup-post-os.sh
@@ -10,4 +10,17 @@ if [ $?	-eq 0 ]; then
     vim +PlugInstall +qall
 fi
 
+# update user favs
 xdg-user-dirs-update
+
+# link dirs for root user
+sudo su
+mkdir ${HOME}/.vimviews ${HOME}/.vimbackup ${HOME}/.vimswap
+ln -s ~thesaneone/.vim* .
+ln -s ~thesaneone/.zsh* .
+ln -s ~thesaneone/.zfun* .
+ln -s ~thesaneone/.fzf* .
+
+# install node/pip packages
+# diff-so-fancy, ipython, mechanize
+
