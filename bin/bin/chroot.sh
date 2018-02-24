8ccfc620f412a300542c1e41b549c6228f8108d7
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2018-02-24 21:42:01 +0530

    committing

--- bin/bin/chroot.sh
+++ bin/bin/chroot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo mount -o bind /proc $1/proc
-sudo mount -o bind /dev $1/dev
-sudo mount -o bind /dev/pts $1/dev/pts
-sudo mount -o bind /sys $1/sys
-sudo cp /etc/resolv.conf $1/etc/resolv.conf
-sudo chroot $1 /bin/bash
+mount -o bind /proc $1/proc
+mount -o bind /dev $1/dev
+mount -o bind /dev/pts $1/dev/pts
+mount -o bind /sys $1/sys
+cp /etc/resolv.conf $1/etc/resolv.conf
+chroot $1 /bin/bash
