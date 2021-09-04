dfca8d3a99a12d7564ba9c4d2cfd3b5480185a71
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2021-09-04 22:48:11 +0530

    moar

--- bin/bin/backup-data.sh
+++ bin/bin/backup-data.sh
@@ -3,7 +3,7 @@
 if $(mountpoint -q /data) && $(mountpoint -q /tv) 
 then
     logger -t "BACKUPDATA"  -p WARN "mounted - proceeding"
-    flock -n /tmp/lock_data_sync -c "/home/thesaneone/bin/mirror_one_way -l /data/ /tv/data1"
+    flock -n /tmp/lock_data_sync -c "${HOME}/bin/mirror_one_way -l /data/ /tv/data1"
 else
     logger -t "BACKUPDATA"  -p WARN "not mounted - not backingup"
 fi
--- bin/bin/backup-movies.sh
+++ bin/bin/backup-movies.sh
@@ -3,7 +3,7 @@
 if $(mountpoint -q /movies) && $(mountpoint -q /movies1) 
 then
     logger -t "BACKUPMOVIES"  -p WARN "not mounted - not backingup"
-    flock -n /tmp/lock_movies_sync -c "/home/thesaneone/bin/mirror_one_way -l /movies/ /movies1"
+    flock -n /tmp/lock_movies_sync -c "${HOME}/bin/mirror_one_way -l /movies/ /movies1"
 else
     logger -t "BACKUPMOVIES"  -p WARN "not mounted - not backingup"
 fi
