2b16128e8c4f065544fef86cf3d471feb1a47295
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2026-01-28 23:27:47 +0530

    moar

--- bin/bin/setup-post-os.sh
+++ bin/bin/setup-post-os.sh
@@ -24,13 +24,16 @@ ln -s ~${USER}/.zfun* .
 ln -s ~${USER}/.fzf* .
 
 echo "Here are things you might want to do: 
-    - unfuck dotfiles stowing
-	- asdf install: yarn, golang, python, node
-	- pip install: pgcli, ipython, pynvim
-	- yarn install: diff-so-fancy
-	- setup neovim: checkhealth
-	- postgres, redis
-	- syncthing setup
-	- playx + instantmusic + musickube
-	- lsd
-"
+    x unfuck dotfiles stowing
+    x setup qbittorrent-nox svc
+    x cp vlc prefs
+    x install dns config
+    x install deno
+    x install yt-dlp
+    x install sonarr
+    x install jackett
+    x setup backup crond
+    - setup swapfile
+    x get kdeconnect working
+    x disable xscreensaver timeout
+    x install jellyfin + config apps"
